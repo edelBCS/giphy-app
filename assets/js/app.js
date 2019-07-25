11ec66d0cd30537d8781b4024de1ea3ba12f552c
edelBCS <edelm8@gmail.com>
2019-07-24 23:09:55 -0700

    add ability to start/stop gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,54 @@
-$(document).on("click", "#gifBtns button", function() {
+$(document).on("click", "#gifBtns button", function () {
     // Grabbing and storing the data-animal property value from the button
     var animal = $(this).attr("data-animal");
 
     // Constructing a queryURL using the animal name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      animal + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        animal + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     // Performing an AJAX request with the queryURL
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After data comes back from the request
-      .then(function(response) {
-        console.log(queryURL);
+            url: queryURL,
+            method: "GET"
+        })
+        // After data comes back from the request
+        .then(function (response) {
+            console.log(queryURL);
 
-        console.log(response);
-        // storing the data from the AJAX request in the results variable
-        var results = response.data;
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
 
-        // Looping through each result item
-        for (var i = 0; i < results.length; i++) {
+            // Looping through each result item
+            for (var i = 0; i < results.length; i++) {
 
-          // Creating and storing a div tag
-          var animalDiv = $("<div>");
+                // Creating and storing a div tag
+                var animalDiv = $("<div>");
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + results[i].rating);
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + results[i].rating);
 
-          // Creating and storing an image tag
-          var animalImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          animalImage.attr("src", results[i].images.fixed_height.url);
+                // Creating and storing an image tag
+                var animalImage = $("<img>");
+                // Setting the src attribute of the image to a property pulled off the result item
+                animalImage.attr("src", results[i].images.fixed_height_still.url);
+                animalImage.attr("data-animate", results[i].images.fixed_height.url);
+                animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+                animalImage.attr("data-state", "still");
 
-          // Appending the paragraph and image tag to the animalDiv
-          animalDiv.append(p);
-          animalDiv.append(animalImage);
+                // Appending the paragraph and image tag to the animalDiv                
+                animalDiv.append(animalImage);
+                animalDiv.append(p);
 
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          $("#gifArea").prepend(animalDiv);
-        }
-      });
-  });
+                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                $("#gifArea").prepend(animalDiv);
+            }
+        });
+});
 
-  $("#addGIFbtn").on("click", function(){
+$("#addGIFbtn").on("click", function () {
     console.log($("#newBtnText").val())
-    if ($("#newBtnText").val() != ""){
+    if ($("#newBtnText").val() != "") {
         var newBtn = $("<li>")
             .append($("<button>")
                 .addClass("btn btn-light col-12 my-1")
@@ -54,4 +57,10 @@ $(document).on("click", "#gifBtns button", function() {
         $("#gifBtns").append(newBtn);
         $("#newBtnText").val("")
     }
-  });
+});
+
+$(document).on("click", "img", function(){
+    ($(this).attr("data-state") === "still")?
+        $(this).attr("src", $(this).attr("data-animate")).attr("data-state", "animate"):
+        $(this).attr("src", $(this).attr("data-still")).attr("data-state", "still");
+});
